0c8c4c4f8841f448c366c7519080dbe66e6b323e
BrunoDS <bruno.diego.x@hotmail.com>
2022-07-21 09:25:08 +0000

    add color to error message

--- welcome.py
+++ welcome.py
@@ -7,25 +7,26 @@ from typingMode import typingMode
 
 def welcome():
     """This function prints the welcome message to the user"""
-    titleFont = Figlet(font="doom")
-    subtitleFont = Figlet(font="digital")
-    firstLine = "Welcome to\n"
-    gameTitle = titleFont.renderText("hangman game")
-    typingMode(Fore.LIGHTBLUE_EX + firstLine + Fore.RESET)
+    title_font = Figlet(font="doom")
+    subtitle_font = Figlet(font="digital")
+    first_line = "Welcome to\n"
+    game_title = title_font.renderText("hangman game")
+    typingMode(Fore.LIGHTBLUE_EX + first_line + Fore.RESET)
     print()
-    print(Fore.LIGHTRED_EX + gameTitle + Fore.RESET)
+    print(Fore.LIGHTRED_EX + game_title + Fore.RESET)
     sleep(0.5)
     print()
 
     while True:
         user_name = input("Please enter your name: \n")
         if str.isalpha(user_name):
-            welcomeMessage = subtitleFont.renderText(f"Welcome \
+            welcome_message = subtitle_font.renderText(f"Welcome \
 to the hangman game, {user_name}")
-            print(Fore.LIGHTBLUE_EX + welcomeMessage + Fore.RESET)
+            print(Fore.LIGHTBLUE_EX + welcome_message + Fore.RESET)
             break
         elif len(user_name) == 0:
-            print("Insufficient characters.\
-             Please enter your name or nickname.")
+            ins_char = "Insufficient characters."
+            print(Fore.LIGHTRED_EX + ins_char + Fore.RESET)
+            print("Please enter your name or nickname.\n")
         else:
             print("Only letters are allowed")
